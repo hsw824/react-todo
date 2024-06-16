c6e7bb021886450327e6f300bbf5bb8bbe01693d
hsw824 <ghdtjddn0824@naver.com>
2024-06-16 16:52:49 +0900

    더 해보기

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
+
+// TODO:useReducer사용해보기
+// TODO: 완료로 취소선 만들어보고 총 개수에 완료된 개수 추가해보기
+// TODO: 컴포넌트 조금 더 나눠보기
 export interface TodoType {
   id: number;
   text: string;
